fb5be3cf6d05040f6795087092f34b28ebb484cf
rutuja_mali <rutujamali111@gmail.com>
2022-08-06 20:58:55 +0530

    Edit method

--- AddressBook.java
+++ AddressBook.java
@@ -1,6 +1,7 @@
 package addressbooksystem;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -46,6 +47,50 @@ public class AddressBook {
 		String total=person1.toString()+address.toString();
 		System.out.println(total);
 	}
+	
+	public void edit() {
+		System.out.println("Enter your First name:");
+		String fname = s.next();
+
+		Iterator<Contact> iterator = list.listIterator();
+
+		while (iterator.hasNext()) {
+			Contact person = iterator.next();
+
+			if (fname.equalsIgnoreCase(person.getFname())) {
+				Address address = person.getAddress();
+				System.out.println("Choose field you want to edit:");
+				System.out
+						.println("1.Last Name\t2.Phone Number\t3.City\t4.Zip\t5. State");
+				switch (s.nextInt()) {
+				case 1:
+					System.out.println("Re-Correct your lastname");
+					person.setLname(s.next());
+					break;
+				case 2:
+					System.out.println("Re-Correct your Phone Number");
+					person.setPhonenumber(s.nextLong());
+					break;
+				case 3:
+					System.out.println("Re-Correct your City");
+					address.setCity(s.next());
+					break;
+				case 4:
+					System.out.println("Re-Correct your Zip");
+					address.setZip(s.nextLong());
+					break;
+				case 5:
+					System.out.println("Re-Correct your State");
+					address.setState(s.next());
+					break;
+				}
+
+			}
+		}
+		System.out.println("contact Edited");
+
+	}
+
 
 
 }
