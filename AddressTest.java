cfc63dab1ac38c4a08c22f085d625a586bc067ec
rutuja_mali <rutujamali111@gmail.com>
2022-08-06 20:24:34 +0530

    Added contact

---
+++ AddressTest.java
@@ -0,0 +1,13 @@
+package addressbooksystem;
+
+public class AddressTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddressBook book1=new AddressBook();
+		book1.add();
+		book1.PrintContact();
+
+	}
+
+}
