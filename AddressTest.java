07826ef6131096afefb4e2dd6117f743f953161c
rutuja_mali <rutujamali111@gmail.com>
2022-08-07 07:21:38 +0530

    added multiple records at a time

--- AddressTest.java
+++ AddressTest.java
@@ -1,17 +1,49 @@
 package addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AddressBook book1=new AddressBook();
-		book1.add();
-		book1.PrintContact();
-//		book1.edit();
-//		book1.PrintContact();
-		book1.delete();
-		book1.PrintContact();
-		
+		Scanner sc=new Scanner(System.in);
+		int option;
+		do
+		{
+			System.out.println("--- Address Book Management ---\n");
+            System.out.println("\t--MENU--");
+			System.out.println("Enter operation number..");
+			option=sc.nextInt();
+			if(option==0)
+			{
+				System.exit(0);
+			}
+			switch (option)
+			{
+			case 1:
+				book1.add();
+				break;
+			case 2:
+				book1.PrintContact();
+				break;
+			case 3:
+				book1.edit();
+				break;
+			case 4:
+				book1.delete();
+				break;
+			default:
+				break;
+			}
+			 System.out.println("Enter zero to exit     \n");
+            System.out.println("1: Add New Person      ");
+            System.out.println("2: Display Records     ");
+            System.out.println("3: Edit Person     ");
+            System.out.println("4: Delete Person     ");
+           
+		}
+		while(option!=0);
 	}
 
 }
